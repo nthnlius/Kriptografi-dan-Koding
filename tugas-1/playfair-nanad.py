ac23d203d9d01a4b2cda0ce7c1092c44c3155d51
nadyalaurentia <nadyalaurentia@gmail.com>
2022-02-02 20:31:14 +0700

    playfair final

--- tugas-1/playfair-nanad.py
+++ tugas-1/playfair-nanad.py
@@ -1,88 +1,131 @@
-##Pembuatan Key Matrix
-from sqlalchemy import false
+##Fungsi Pembuatan Key Matrix
+def key_mtx(key):
+    key = key.replace(" ", "")
+    key = key.upper()
+
+    keylist = list()
+    for x in key:
+        if x not in keylist:
+            if x != 'J':
+                keylist.append(x)
+
+    for i in range(65, 91):
+        if chr(i) not in keylist:
+            if chr(i) != 'J':
+                keylist.append(chr(i))
+
+    matrix = [[0 for i in range(5)] for i in range(5)]
+    id = 0
+    for i in range(5):
+        for j in range(5):
+            matrix[i][j] = keylist[id]
+            id += 1
+    
+    return matrix
 
 
-key = input("Masukkan key: ")
-key = key.replace(" ", "")
-key = key.upper()
+##Fungsi Playfair
+def playfair(method, matrix, input, output):
+        c = 0
+        output = ""
 
-keylist = list()
-for x in key:
-    if x not in keylist:
-        if x != 'J':
-            keylist.append(x)
+        while c < len(input):
+            i1 = 0
+            j1 = 0
+            i2 = 0
+            j2 = 0
+            found1 = False
+            found2 = False
 
-for i in range(65, 91):
-    if chr(i) not in keylist:
-        if chr(i) != 'J':
-            keylist.append(chr(i))
+            while (found1 and found2) is False: 
+                for i in range(5):
+                    for j in range(5):
+                        if (found1 and found2) is False:
+                            if matrix[i][j] == input[c]:
+                                i1 = i
+                                j1 = j
+                                found1 = True
+                            elif matrix[i][j] == input[c+1]:
+                                i2 = i
+                                j2 = j
+                                found2 = True
 
-matrix = [[0 for i in range(5)] for i in range(5)]
-id = 0
-for i in range(5):
-    for j in range(5):
-        matrix[i][j] = keylist[id]
-        id += 1
+            if i1 == i2:
+                if (method == "E"):
+                    output += matrix[i1][(j1+1) % 5]
+                    output += matrix[i2][(j2+1) % 5]
+                else:
+                    output += matrix[i1][(j1-1) % 5]
+                    output += matrix[i2][(j2-1) % 5]
+            elif j1 == j2:
+                if (method == "E"):
+                    output += matrix[(i1+1) % 5][j1]
+                    output += matrix[(i2+1) % 5][j2]
+                else:
+                    output += matrix[(i1-1) % 5][j1]
+                    output += matrix[(i2-1) % 5][j2]
+            else:
+                output += matrix[i1][j2]
+                output += matrix [i2][j1]
 
-print(matrix)
+            c += 2
+
+        if (method == "E"):
+            print("Ciphertext: ", output)
+        else:
+            print("Plaintext: ", output)
 
 
+##Pilih enkripsi atau dekripsi
+method = input("Pilih enkripsi atau dekripsi (E/D): ")
 
-##Pengolahan plaintext
-pt = input("Masukkan plaintext: ")
-pt = pt.replace(" ", "")
-pt = pt.upper()
-pt = pt.replace("J", "I")
+stop = False
+while not stop:
+    ##Enkripsi:
+    if (method.upper() == "E"):
+        pt = input("Masukkan plaintext: ")        
+        key = input("Masukkan key: ")
+        matrix = key_mtx(key)
+        
+        ##Pengolahan plaintext
+        pt = pt.replace(" ", "")
+        pt = pt.upper()
+        pt = pt.replace("J", "I")
 
-done = False
-c = 0
-while not done:
-    if c >= len(pt) - 1:
-        done = True
+        done = False
+        c = 0
+        while not done:
+            if c >= len(pt) - 1:
+                done = True
+            else:
+                if pt[c] == pt[c+1]:
+                    pt = pt[:c+1] + 'X' + pt[c+1:]
+                c += 2
+
+        if (len(pt) % 2) == 1:
+            pt = pt + 'X'
+
+        ##Proses Enkripsi
+        cipher = ""
+        playfair("E", matrix, pt, cipher)
+
+
+    ##Dekripsi:
+    elif (method.upper() == "D"):
+        ct = input("Masukkan ciphertext: ")        
+        key = input("Masukkan key: ")
+        matrix = key_mtx(key)
+        
+        ##Pengolahan ciphertext
+        ct = ct.replace(" ", "")
+        ct = ct.upper()
+
+        ##Proses Dekripsi
+        plain = ""
+        playfair("D", matrix, ct, plain)
+
+    stp = input("Stop (Y/N)? ")
+    if (stp.upper() == "Y"):
+        stop = True
     else:
-        if pt[c] == pt[c+1]:
-            pt = pt[:c+1] + 'X' + pt[c+1:]
-        c += 2
-
-if (len(pt) % 2) == 1:
-    pt = pt + 'X'
-
-##Enkripsi
-c = 0
-cipher = ""
-
-while c < len(pt):
-    i1 = 0
-    j1 = 0
-    i2 = 0
-    j2 = 0
-    found1 = False
-    found2 = False
-    
-    while found1 or found2 is False: 
-        for i in range(5):
-            for j in range(5):
-                if matrix[i][j] == pt[c]:
-                    i1 = i
-                    j1 = j
-                    found1 = True
-                    break
-                elif matrix[i][j] == pt[c+1]:
-                    i2 = i
-                    j2 = j
-                    found2 = True
-                    break
-    
-    if i1 == i2:
-        cipher += matrix[i1][j1+1 % 5]
-        cipher += matrix[i2][j2+1 % 5]
-    elif j1 == j2:
-        cipher += matrix[i1+1 % 5][j1]
-        cipher += matrix[i2+1 % 5][j2]
-    else:
-        cipher += matrix[i1][j2]
-        cipher += matrix [i2][j1]
-    
-    c += 2
-
-print(cipher)
+        method = input("Pilih enkripsi atau dekripsi (E/D): ")
