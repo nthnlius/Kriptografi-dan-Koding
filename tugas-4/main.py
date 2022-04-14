b82e006f8aed6c8f79568b26c341f7c90252e4b4
nadyalaurentia <nadyalaurentia@gmail.com>
2022-04-14 20:37:38 +0700

    tugas-4

---
+++ tugas-4/main.py
@@ -0,0 +1,253 @@
+from cProfile import label
+import sys
+import random
+import os.path
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIntValidator
+from PyQt5.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QRadioButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
+    QPlainTextEdit,
+    QFileDialog,
+    QStackedWidget,
+    
+)
+
+from rsa import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Tugas Kecil 3 II4031 Kriptografi dan Koding")
+        self.setStyleSheet("font: 10pt Verdana;\n"
+                            "background-color: #8ecae6")
+        
+        self.labeltitle = QLabel("Implementasi Algoritma RSA")
+        self.labeltitle.setStyleSheet("font: bold 10pt Verdana")
+        self.labelsubtitle = QLabel("By: Nathaniel & Nadya\n")
+        self.labelsubtitle.setStyleSheet("font: 10pt Verdana")
+
+        self.labelinput = QLabel("Input:")
+        self.chooseinputfile = QPushButton("Choose file")
+        self.chooseinputfile.clicked.connect(self.open_input)
+        self.chooseinputfile.setStyleSheet("background-color: #023047;\n"
+                                            "color: white")
+
+        self.inputdisplay = QPlainTextEdit()
+        self.inputdisplay.setReadOnly(True)
+        self.inputdisplay.setStyleSheet("background-color: white")
+        self.inputfield = QPlainTextEdit()
+        self.inputfield.setReadOnly(True)
+        self.inputfield.setStyleSheet("background-color: white")
+        
+        self.label1 = QLabel(" ")        
+        layoutkey = QVBoxLayout()
+        self.labelkey = QLabel("e or d:")
+        self.inputkey = QPlainTextEdit()
+        self.inputkey.setStyleSheet("background-color: white")
+        layoutkey.addWidget(self.labelkey)
+        layoutkey.addWidget(self.inputkey)
+
+        layoutn = QVBoxLayout()
+        self.labeln = QLabel("n:")
+        self.inputn = QPlainTextEdit()
+        self.inputn.setStyleSheet("background-color: white")
+        layoutn.addWidget(self.labeln)
+        layoutn.addWidget(self.inputn)
+
+        self.stack1 = QWidget()
+        self.stack1.setLayout(layoutkey)
+        self.stack2 = QWidget()
+        self.stack2.setLayout(layoutn)
+        layouth = QHBoxLayout()
+        layouth.addWidget(self.stack1)
+        layouth.addWidget(self.stack2)
+
+        self.keyfile = QPushButton("Choose key file")
+        self.keyfile.clicked.connect(self.open_key)
+        self.keyfile.setStyleSheet("background-color: #023047;\n"
+                                    "color: white")
+        
+        self.label1 = QLabel(" ")
+        self.labelgen = QLabel("Do not have key?")
+        self.genkey = QPushButton("Generate key first")
+        self.genkey.clicked.connect(self.gen)
+        self.genkey.setStyleSheet("background-color: #023047;\n"
+                                    "color: white")
+        
+        self.label2 = QLabel(" ")
+        self.labeloutput = QLabel("Output:")
+        self.outputfield = QPlainTextEdit()
+        self.outputfield.setReadOnly(True)
+        self.outputfield.setStyleSheet("background-color: white")
+        self.outputtime = QLabel("")
+        self.outputsize = QLabel("")
+
+        self.saveoutput = QPushButton("Download")
+        self.saveoutput.clicked.connect(self.savefile)
+        self.saveoutput.setStyleSheet("background-color: #023047;\n"
+                                            "color: white")
+
+        self.orioutput = QPlainTextEdit()
+        
+        self.encrypt = QPushButton("Encrypt")
+        self.encrypt.clicked.connect(self.encrypt_function)
+        self.decrypt = QPushButton("Decrypt")
+        self.decrypt.clicked.connect(self.decrypt_function)
+
+        self.encrypt.setStyleSheet("background-color : #fb8500")
+        self.decrypt.setStyleSheet("background-color : #ffb703")
+
+        widget = QWidget()
+        layoutall = QVBoxLayout()
+        layoutall.addWidget(self.labeltitle)
+        layoutall.addWidget(self.labelinput)
+        layoutall.addWidget(self.chooseinputfile)
+        # layoutall.addWidget(self.inputfield)
+        layoutall.addWidget(self.inputdisplay)
+        self.stack = QWidget()
+        self.stack.setLayout(layouth)
+        layoutall.addWidget(self.stack)
+        layoutall.addWidget(self.keyfile)
+        layoutall.addWidget(self.label1)
+        layoutall.addWidget(self.labelgen)
+        layoutall.addWidget(self.genkey)
+        layoutall.addWidget(self.label2)
+        layoutall.addWidget(self.labeloutput)
+        layoutall.addWidget(self.outputfield)
+        layoutall.addWidget(self.outputtime)
+        layoutall.addWidget(self.outputsize)
+        layoutall.addWidget(self.saveoutput)
+        layoutall.addWidget(self.encrypt)
+        layoutall.addWidget(self.decrypt)
+        
+        widget.setLayout(layoutall)
+        self.setCentralWidget(widget)
+        self.rsa = RSA()
+        self.encryptmsg = ''
+    def gen(self):
+        self.rsa.genKey()
+        self.genkey.setText("Generated!")
+
+    def encrypt_function(self):
+        # self.outputfield.setPlainText("")
+        # self.outputtime.setText("")
+        # self.outputsize.setText("")
+        inputtext = self.inputfield.toPlainText()
+
+        with open(inputtext, 'rb') as f:
+            byte = f.read()
+        input_size = len(byte)
+        self.rsa.e = int(self.inputkey.toPlainText())
+        self.rsa.n = int(self.inputn.toPlainText())
+        start_time = time()
+        encrypted = self.rsa.encrypt(byte)
+        end_time = time()
+        self.outputtime.setText("Time taken for encrypting : " + str(end_time - start_time) + " seconds")
+        self.outputsize.setText ("Size of plaintext file: "+ str(input_size * 16) + " bytes")
+        nani = ""
+        for i in range (len (encrypted)):
+            text = hex(encrypted[i])
+            if (len(text)< 34):
+                text2 = text[0:2]+('0'*(34-len(text)))+text[2:len(text)]
+            else :
+                text2 = text
+            for j in range (1, len(text2)//2):
+                euy = text2[j*2 :j*2+2] #euy mengambil tiap bytes dalam text.
+                ahh = int(euy, 16) #ahh mengubah euy dari heksadesimal jadi integer
+                # print ("euy: ", euy,"ahh", ahh)
+                nani+=(chr(ahh))
+        self.outputfield.setPlainText(text2)
+        self.encryptmsg = nani
+        # self.genkey.setText("Generate key first")
+
+    def decrypt_function(self):
+        # self.outputfield.setPlainText("")
+        # self.outputtime.setText("")
+        # self.outputsize.setText("")
+        inputtext = self.inputfield.toPlainText()
+
+        with open(inputtext, 'rb') as f:
+            byte = f.read()
+            input_size = len(byte)
+            self.rsa.d = int(self.inputkey.toPlainText())
+            self.rsa.n = int(self.inputn.toPlainText())
+            start_time = time()
+            decrypted = self.rsa.decrypt(byte)
+            end_time = time()
+            self.outputtime.setText("Time taken for decrypting : " + str(end_time - start_time) + " seconds")
+            self.outputsize.setText("Size of plaintext file:" + str(input_size / 16) + " bytes")
+            self.outputfield.setPlainText(decrypted)
+            self.encryptmsg=decrypted
+        # self.genkey.setText("Generate key first")
+
+    def open_input(self):
+        fileName = ''
+        fileName, _ = QFileDialog.getOpenFileName(self, 'File Input')
+        content = ''
+        self.outputfield.clear()
+        if fileName:
+            if fileName.endswith('.txt'):
+                # File txt
+                with open(fileName, 'r', encoding='ISO-8859-1') as f:
+                    content = f.read()
+                    self.inputfield.setPlainText(fileName)
+                    self.inputdisplay.setPlainText(content + "\nFrom: " + fileName)
+            else:
+                self.inputfield.setPlainText(fileName)
+                self.inputdisplay.setPlainText("From: " + fileName)
+
+    def open_key(self):
+        fileName = ''
+        fileName, _ = QFileDialog.getOpenFileName(self, 'File Key')
+        content = ''
+        d = ''
+        e =''
+        n = ''
+        i = 6
+        self.outputfield.clear()
+        if fileName:
+            if fileName.endswith('.pri'):
+                f = open(fileName)
+                private = json.load(f)
+                self.rsa.d = private['d']
+                self.rsa.n = private['n']
+                f.close()
+                self.inputkey.setPlainText(str(self.rsa.d))
+                self.inputn.setPlainText(str(self.rsa.n))
+            elif fileName.endswith('.pub'):
+                f = open(fileName)
+                public = json.load(f)
+                self.rsa.e = public['e']
+                self.rsa.n = public['n']
+                f.close()
+                self.inputkey.setPlainText(str(self.rsa.e))
+                self.inputn.setPlainText(str(self.rsa.n))
+                              
+
+    def savefile(self):
+        fileName, _ = QFileDialog.getSaveFileName(self, 'Save Output', 'output')
+        if(fileName):
+            output = self.encryptmsg
+            output2 = bytearray(output, encoding="iso8859")
+            fname = open(fileName, 'wb')
+            fname.write(output2)
+            fname.close()
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.resize(1000,800)
+window.show()
+
+app.exec()
---
+++ tugas-4/rsa.py
@@ -0,0 +1,163 @@
+'''program ini diciptakan untuk mencoba melakukan enkripsi menggunakan RSA'''
+from time import time
+from math import ceil, floor, sqrt, gcd, log
+from typing import Tuple
+import sympy
+import sys
+import json
+def isPrime(rndint) -> bool:
+    if rndint>=10 :
+        for i in range (2, ceil(sqrt(rndint))+1):
+            if (rndint%i == 0):
+                return False
+        return True
+    elif rndint >0 and rndint<10 :
+        for i in range (2, rndint):
+            if (rndint%i == 0):
+                return False
+        return True
+    else :
+        return isPrime((rndint)*(-1))
+class RSA:
+    def __init__(self):
+        try :
+            with open("RSA-NN.pub", "r")as r:
+                pubkey = json.load(r)
+                self.e = pubkey['e']
+                self.n = pubkey['n']
+            with open("RSA-NN.pri", "r")as r:
+                prikey = json.load(r)
+                self.d = prikey['d']
+        except FileNotFoundError :
+            p = sympy.randprime(2**63, 2**64-1)
+            q = sympy.randprime(2**63, 2**64-1)
+            while p==q :
+                q = sympy.randprime(2**63, 2**64-1)
+            self.n = p*q
+            self.tot = (p-1)*(q-1)
+            self.generateE()
+            self.generateD()
+            tup = {'d':self.d, 'n':self.n}
+            with open("RSA-NN.pri" , "w")as f:
+                json.dump(tup, f)
+            tup = {'e':self.e, 'n':self.n}
+            with open ("RSA-NN.pub", "w")as f:
+                json.dump(tup, f)
+    
+    def isEallowed(self, e)-> bool:
+        if (isPrime(self.p) and isPrime(self.q)):
+            return gcd(e, self.tot) == 1
+    def generateE(self)->None:
+        randnum = sympy.randprime(2**63, 2**64-1)
+        while (gcd(randnum, self.tot)!=1):
+            randnum = sympy.randprime(2**63, 2**64-1)
+        self.e = randnum
+    ''' rumus : d = (1+k*tot)/e'''
+    def setE(self,num):
+        if (self.isEallowed(num)):
+            self.e=num
+            print ("E has been set")
+        else :
+            print("You have chosen the wrong E")
+    def genKey (self):
+        p = sympy.randprime(2**63, 2**64-1)
+        q = sympy.randprime(2**63, 2**64-1)
+        while p==q :
+            q = sympy.randprime(2**63, 2**64-1)
+        self.n = p*q
+        self.tot = (p-1)*(q-1)
+        self.generateE()
+        self.generateD()
+        tup = {'d':self.d, 'n':self.n}
+        with open("RSA-NN.pri" , "w")as f:
+            json.dump(tup, f)
+        tup = {'e':self.e, 'n':self.n}
+        with open ("RSA-NN.pub", "w")as f:
+            json.dump(tup, f)        
+    def generateD(self)-> int:
+        d = pow(self.e, -1, self.tot)
+        print("d has been set to : ", d)
+        self.d = d
+        return d
+    def encrypt(self, message:bytearray):
+        ciphertext = []
+
+        for byt in message :
+            # print (byte)
+            ciphermsg = pow(byt, self.e, self.n)
+            ciphertext.append(ciphermsg)
+        # print (ciphertext)
+        # for i in range (len(ciphertext)):
+        #     print (hex(ciphertext[i]))
+        # print (ciphertext)
+        return ciphertext
+    def decrypt (self, ciphermsg : bytearray):
+        plaintext = []
+        plaintxt = ''
+        # print(len(ciphermsg))
+        for i in range (0, len(ciphermsg), 16):
+            print ("i :", i)
+            # print ("ciphermsg : ", ciphermsg)
+            ciphertext = bytearray(ciphermsg)
+            # print(type(ciphertext)) mengeluarkan bytearray
+            text9 = int.from_bytes(ciphertext[i:i+16], byteorder="big", signed=False)
+            # print ("aaa" , ciphertext[i*16:i*16+16])
+            # print(f'hex: {hex(text9)}')
+            # print("text : ", text9)
+            # for j in range (len(text9)):
+            plainmsg = pow(text9, self.d, self.n)
+            # print("plainmsg : ", plainmsg)
+            plaintext.append(plainmsg)
+            plaintxt+=chr(plainmsg)
+        # print (plaintxt)
+        
+        return (plaintxt)
+
+def nextPrime(x):
+    nextprime = x + 1
+    while (not isPrime(nextprime)):
+        print(nextprime, "is not a prime number")
+        nextprime +=1
+    print(nextprime, "is prime number")
+    return nextprime
+
+def isEven(x : int)->bool :
+    return x%2==0
+def exp2(a:int , b:int)-> int :
+    if b == 0:
+        return 1
+    else :
+        x = exp2(a, b//2)
+        if (isEven(b)):
+            return x*x
+        else :
+            return x*x*a
+
+# rsa=RSA()
+# fileName = "/home/hydroxideacid/kuliah/kripto/tugas/tugas-3/output"
+# with open("ASU.txt", "rb") as f :
+#     fileraw = f.read()
+# ciphertext = rsa.encrypt(fileraw)
+# nani = ""
+# check=[]
+# for i in range (len (ciphertext)):
+#     text = hex(ciphertext[i])
+#     if (len(text)< 34):
+#         # print (text[0:2])
+#         text2 = text[0:2]+('0'*(34-len(text)))+text[2:len(text)]
+#     else :
+#         text2 = text
+#     for j in range (1, len(text2)//2):
+#         euy = text2[j*2 :j*2+2] #euy mengambil tiap bytes dalam text.
+#         ahh = int(euy, 16) #ahh mengubah euy dari heksadesimal jadi integer
+#         nani+=(chr(ahh))
+#         check.append(ahh)
+# with open(fileName, "rb") as f:
+#     file = f.read()
+# ciphertext = rsa.decrypt(file)
+#72074772781006188336026735860538711311
+#72074772781006188336062764657557675279
+#0x363918a7fe74fc1f3b20f0695cec9d0f
+#108898005206395243020851411875449083296
+#108898005206395243020851411875449083168
+
